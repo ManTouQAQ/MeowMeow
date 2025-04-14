9057d39dc5c9cd68b287f00f40dce5608486147b
ManTou <69822848+ManTouQAQ@users.noreply.github.com>
2025-04-14 17:48:55 +0800

    :poop: fix rotate block overlapped bug but fixed code is soooooooooooooo bad

--- src/main/kotlin/me/mantou/meow/placer/task/RotateBlockTask.kt
+++ src/main/kotlin/me/mantou/meow/placer/task/RotateBlockTask.kt
@@ -15,7 +15,7 @@ class RotateBlockTask(
     private val pos2: Location,
     private val center: Location,
     private val angdeg: Double,
-    private val axis : Vector3i
+    private val axis: Vector3i
 ) : BlockTask {
     override fun accept(history: RegionHistory) {
         history.start(pos1.world!!)
@@ -36,6 +36,7 @@ class RotateBlockTask(
         val model = Matrix3f()
         model.rotate(-Math.toRadians(angdeg).toFloat(), Vector3f(axis))
 
+        val overlapBuffer = mutableSetOf(*needRotateBlocks.toTypedArray())
         for (needMoveSnapshot in needRotateBlocks) {
             val relativePos = Vector3f(
                 needMoveSnapshot.pos.x.toFloat() - center.blockX,
@@ -50,16 +51,34 @@ class RotateBlockTask(
             val newZ = center.blockZ + Math.round(relativePos.z)
 
             val block = pos1.world!!.getBlockAt(newX, newY, newZ)
+            val newPos = Vector3i(newX, newY, newZ)
 
-            val originalSnapshot = needRotateBlocks.find { it.pos == Vector3i(newX, newY, newZ) }
-            if (originalSnapshot != null){
-                history.addSnapshot(newX, newY, newZ, originalSnapshot.from, needMoveSnapshot.from, originalSnapshot.fromData, needMoveSnapshot.fromData)
-            }else{
-                history.addSnapshot(newX, newY, newZ, block.type, needMoveSnapshot.from, block.blockData, needMoveSnapshot.fromData)
+            // TODO 这段太狗屎了 用来处理多个方块旋转到一起的情况
+            val overlapped = overlapBuffer.find { it.pos == newPos }
+            val snapshot = if (overlapped != null) {
+                history.addSnapshot(
+                    newX,
+                    newY,
+                    newZ,
+                    overlapped.from,
+                    needMoveSnapshot.from,
+                    overlapped.fromData,
+                    needMoveSnapshot.fromData
+                )
+            } else {
+                history.addSnapshot(
+                    newX,
+                    newY,
+                    newZ,
+                    block.type,
+                    needMoveSnapshot.from,
+                    block.blockData,
+                    needMoveSnapshot.fromData
+                )
             }
+            overlapBuffer.add(snapshot)
 
             block.setType(needMoveSnapshot.from, false)
-
             needMoveSnapshot.fromData?.apply {
                 block.setBlockData(this, false)
             }
